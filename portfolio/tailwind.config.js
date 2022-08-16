c1647568fa6a2ef0a9faee7f70a9ae61be72b1e2
argianardi <argianardi1435@gmail.com>
2022-08-16 17:05:05 +0700

    add cutom container, break point and color

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14B8A6",
+        dark: "#0f1721",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
